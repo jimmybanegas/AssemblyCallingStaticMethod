9deb51f6effbc52a0c41b6258ada18859c57da40
Jimmy Ramos <jimmy.ramos@unitec.edu>
2015-03-04 21:01:59 -0600

    Init

---
+++ MainAssembly/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainAssembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Path de dll: ");
+
+            var path = Console.ReadLine();
+
+            var dll = Assembly.LoadFile(path);
+
+
+            var list = dll.ExportedTypes;
+
+
+            foreach (var tipo in list)
+            {
+
+                var methods = tipo.GetMethods();
+
+                foreach (var m in methods)
+                {
+                    if (m.Name == "Ejecutar")
+                    {
+                        var method = tipo.GetMethod("Ejecutar",BindingFlags.Public|BindingFlags.Static);
+
+                        method.Invoke(null,null); // assuming it doesn't take parameters
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
